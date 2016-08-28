a7266cc72538118c6fff0b1af8d50e1faff4e28c
deeart <deeartrama88@gmail.com>
2016-08-29 00:36:06 +0300

    side menu on main page is added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
-/**
- * Created by Denis on 23.08.2016.
- */
+(function () {
+
+    $(document).ready(function () {
+
+        // our side menu all currencies
+        var all_currency_side_menu = $('.side_all_currency_widget');
+        // button to open side menu with all currencies
+        var all_currency_side_menu_open_btn = $('.open_icon_wrapper');
+        // side menu all currencies tabs
+        var all_currency_side_menu_tabs = $('.body_toggle');
+
+
+
+        // open - close side menu
+        $(all_currency_side_menu_open_btn).click(function () {
+            $(all_currency_side_menu).toggleClass('open');
+        });
+
+        // open - close side menu tabs
+        $(all_currency_side_menu_tabs).click(function () {
+             $(this).parent().find('.wrap').slideToggle().parent().find(this).toggleClass('open');
+        });
+
+
+
+    });
+
+
+}());
